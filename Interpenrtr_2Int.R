4a5c48e3597bc37334b1fff26d73d2fc0cc1b0cf
Dingfan X <68703089+DingfanX@users.noreply.github.com>
2024-05-19 16:37:42 -0400

    Update Interpenrtr_2Int.R

--- Interpenrtr_2Int.R
+++ Interpenrtr_2Int.R
@@ -1,17 +1,14 @@
 ##Interpenetrating subsamples for SRS
-###Interpenetrating samples for USGS Pilot study pairs of 2 interpreters
+###Interpenetrating subsample approach for 2 interpreters
 
-dataset <- read.csv("E:/Ph.D in ESF/Research Work/Interpenetrating subsampling/Calculation/Water/2000_Water_Binary.csv")
+dataset <- read.csv("~/2000_Forest_Binary.csv") #read reference label data
 data <- data.frame(p1=c(dataset$Int109_2000), p2=c(dataset$Int110_2000))
 
 ##Setting up variables
 n= 300 ##number of pixels
-#s=10   ##number of trials, used 10, 100, nd 1000 trials in study
-#s=100
-s=1000
-#s=5000
-a<-2
-m<-150
+s=1000 ##number of trials
+a<-2  ##number of interpreters
+m<-150 ## number of pixels assigned to each interpreter
 
 ##Setting up empty Vectors
 variances = rep(NA,s)
@@ -55,8 +52,6 @@ for(j in 1:s){
       c2=c2+1}
   }
   
-#dataRandom1 <- data.frame(data1=c(data1), data2=c(data2))
-#write.csv(dataRandom1,"E:/Ph.D in ESF/Research Work/Interpenetrating subsampling/Results/SRS/CheckRandomWater_Int2_3.csv")
 ##V12 Calculations
   ybar.s1 = mean(data1)
   ybar.s2 = mean(data2)
@@ -65,21 +60,18 @@ for(j in 1:s){
   data.1.msw = (data1 - ybar.s1)^2
   data.2.msw = (data2 - ybar.s2)^2
   
- # MSb[j] = (1/(m*(a-1)))*(((ybar.s1-ybar.total)^2)+((ybar.s2-ybar.total)^2))
   MSb[j] = (m/(a-1))*(((ybar.s1-ybar.total)^2)+((ybar.s2-ybar.total)^2))
   MSw[j] = (1/(a*(m-1)))*(sum(data.1.msw) + sum(data.2.msw))
   p[j] = ((MSb[j]-MSw[j])/m)/(((MSb[j]-MSw[j])/m)+MSw[j])
   
   V12[j] = ((m-1)/m) * ((MSb[j] - MSw[j])/300)
-  SE_V12[j] = sqrt(abs(V12[j])) #we can not take square root of V12 as V12 can be negative in some cases.
-  
+  SE_V12[j] = sqrt(abs(V12[j]))
   
   ##Calculating Interpenetrating variances
   ys1_ys <- ybar.s1 - ybar.total
   ys2_ys <- ybar.s2 - ybar.total
   variances[j] <- (1/(a*(a-1)))*(ys1_ys^2+ys2_ys^2)
   SE[j] <- sqrt(variances[j])
-
 }
 
 ##Calculating final variance
@@ -97,9 +89,8 @@ StDev_p <- sqrt(var(p))
 
 SE2 <- sqrt(var_mean)
 
-
 output = c(var_mean,SE_mean,V12_mean,SE_V12_mean,p_mean,StDev_variances,StDev_SEs,StDev_V12s,StDev_SEs_V12,StDev_p,SE2)
 results <- data.frame(variances=c(variances), SE=c(SE),V12=c(V12),SE_V12<-c(SE_V12), MSb=c(MSb),MSw=c(MSw),pint=c(p))
-write.csv(results,"E:/Ph.D in ESF/Research Work/Interpenetrating subsampling/Results/SRS/NewMSb_ResultsWater_2pairs_PilotInt9_10_s1000.csv")
+write.csv(results,"~/results.csv")
 write.table(output, 'clipboard', sep='\t', row.names=FALSE, col.names=FALSE)
 
